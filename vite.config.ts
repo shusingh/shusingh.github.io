ae10dd55a6c5e12be9e9abad0ae88c80b99648f0
shusingh <ksingh.shubh@gmail.com>
2025-01-15 19:32:24 -0800

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '.',
+  base: './',
   plugins: [react(), tsconfigPaths()],
 });
